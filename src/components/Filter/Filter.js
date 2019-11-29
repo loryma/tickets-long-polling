76572ccdcfc229de7cca7247001ee242faa05994
loryma <lorymalloc@gmail.com>
2019-11-29 15:36:37 +0200

    fix filters for all options choice

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from "./Filter.module.css";
 
-const Filter = ({ onChange, text }) => {
+const Filter = ({ onChange, text, checked }) => {
   return (
     <label className={classes.Filter}>
       <div className={classes.CheckboxWrapper}>
         <input
           className={classes.Checkbox}
           type="checkbox"
+          checked={checked}
           onChange={onChange}
         />
         <span className={classes.Checkmark}></span>
